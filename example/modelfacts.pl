999d7a1873e11fc376ebfa166aff0e6430d3da5c
Shawn Bowers <smbowers@gmail.com>
2015-06-16 12:38:34 -0700

    q1 through q3, still need to do rest of queries, but need to fix corrected_images in fake_recon first

--- example/modelfacts.pl
+++ example/modelfacts.pl
@@ -1,15 +1,15 @@
 
 % FACT: program(program_id, program_name, begin_annotation_id, end_annotation_id).
-program(1, 'simulate_data_collection', 1, 80).
-program(2, 'initialize_run', 16, 19).
-program(3, 'load_screening_results', 20, 26).
-program(4, 'calculate_strategy', 27, 35).
-program(5, 'log_rejected_sample', 36, 41).
-program(6, 'collect_data_set', 42, 54).
-program(7, 'transform_images', 55, 69).
-program(8, 'log_average_image_intensity', 70, 79).
-program(9, 'collect_next_image', 81, 91).
-program(10, 'transform_image', 92, 98).
+program(1, "simulate_data_collection", 1, 80).
+program(2, "initialize_run", 16, 19).
+program(3, "load_screening_results", 20, 26).
+program(4, "calculate_strategy", 27, 35).
+program(5, "log_rejected_sample", 36, 41).
+program(6, "collect_data_set", 42, 54).
+program(7, "transform_images", 55, 69).
+program(8, "log_average_image_intensity", 70, 79).
+program(9, "collect_next_image", 81, 91).
+program(10, "transform_image", 92, 98).
 
 % FACT: workflow(program_id).
 workflow(1).
@@ -28,80 +28,80 @@ has_sub_program(1, 7).
 has_sub_program(1, 8).
 
 % FACT: port(port_id, port_type, port_name, port_annotation_id).
-port(1, 'param', 'cassette_id', 2).
-port(2, 'param', 'sample_score_cutoff', 3).
-port(3, 'in', 'sample_spreadsheet', 4).
-port(4, 'in', 'calibration_image', 6).
-port(5, 'out', 'corrected_image', 8).
-port(6, 'out', 'run_log', 10).
-port(7, 'out', 'collection_log', 12).
-port(8, 'out', 'rejection_log', 14).
-port(9, 'out', 'run_log', 17).
-port(10, 'param', 'cassette_id', 21).
-port(11, 'in', 'sample_spreadsheet', 22).
-port(12, 'out', 'sample_name', 24).
-port(13, 'out', 'sample_quality', 25).
-port(14, 'param', 'sample_score_cutoff', 28).
-port(15, 'in', 'sample_name', 29).
-port(16, 'in', 'sample_quality', 30).
-port(17, 'out', 'accepted_sample', 31).
-port(18, 'out', 'rejected_sample', 32).
-port(19, 'out', 'num_images', 33).
-port(20, 'out', 'energies', 34).
-port(21, 'param', 'cassette_id', 37).
-port(22, 'param', 'rejected_sample', 38).
-port(23, 'out', 'rejection_log', 39).
-port(24, 'param', 'cassette_id', 44).
-port(25, 'param', 'accepted_sample', 45).
-port(26, 'param', 'num_images', 46).
-port(27, 'param', 'energies', 47).
-port(28, 'out', 'sample_id', 48).
-port(29, 'out', 'energy', 49).
-port(30, 'out', 'frame_number', 50).
-port(31, 'out', 'raw_image_path', 51).
-port(32, 'param', 'sample_id', 57).
-port(33, 'param', 'energy', 58).
-port(34, 'param', 'frame_number', 59).
-port(35, 'in', 'raw_image_path', 60).
-port(36, 'in', 'calibration_image', 62).
-port(37, 'out', 'corrected_image', 64).
-port(38, 'out', 'corrected_image_path', 66).
-port(39, 'out', 'total_intensity', 67).
-port(40, 'out', 'pixel_count', 68).
-port(41, 'param', 'cassette_id', 71).
-port(42, 'param', 'sample_id', 72).
-port(43, 'param', 'frame_number', 73).
-port(44, 'in', 'total_intensity', 74).
-port(45, 'in', 'pixel_count', 75).
-port(46, 'in', 'corrected_image_path', 76).
-port(47, 'out', 'collection_log', 77).
-port(48, 'param', 'cassette_id', 82).
-port(49, 'param', 'sample_id', 83).
-port(50, 'param', 'num_images', 84).
-port(51, 'param', 'energies', 85).
-port(52, 'param', 'image_path_template', 86).
-port(57, 'param', 'raw_image_path', 93).
-port(58, 'param', 'corrected_image_path', 94).
-port(59, 'param', 'calibration_image_path', 95).
+port(1, "param", "cassette_id", 2).
+port(2, "param", "sample_score_cutoff", 3).
+port(3, "in", "sample_spreadsheet", 4).
+port(4, "in", "calibration_image", 6).
+port(5, "out", "corrected_image", 8).
+port(6, "out", "run_log", 10).
+port(7, "out", "collection_log", 12).
+port(8, "out", "rejection_log", 14).
+port(9, "out", "run_log", 17).
+port(10, "param", "cassette_id", 21).
+port(11, "in", "sample_spreadsheet", 22).
+port(12, "out", "sample_name", 24).
+port(13, "out", "sample_quality", 25).
+port(14, "param", "sample_score_cutoff", 28).
+port(15, "in", "sample_name", 29).
+port(16, "in", "sample_quality", 30).
+port(17, "out", "accepted_sample", 31).
+port(18, "out", "rejected_sample", 32).
+port(19, "out", "num_images", 33).
+port(20, "out", "energies", 34).
+port(21, "param", "cassette_id", 37).
+port(22, "param", "rejected_sample", 38).
+port(23, "out", "rejection_log", 39).
+port(24, "param", "cassette_id", 44).
+port(25, "param", "accepted_sample", 45).
+port(26, "param", "num_images", 46).
+port(27, "param", "energies", 47).
+port(28, "out", "sample_id", 48).
+port(29, "out", "energy", 49).
+port(30, "out", "frame_number", 50).
+port(31, "out", "raw_image_path", 51).
+port(32, "param", "sample_id", 57).
+port(33, "param", "energy", 58).
+port(34, "param", "frame_number", 59).
+port(35, "in", "raw_image_path", 60).
+port(36, "in", "calibration_image", 62).
+port(37, "out", "corrected_image", 64).
+port(38, "out", "corrected_image_path", 66).
+port(39, "out", "total_intensity", 67).
+port(40, "out", "pixel_count", 68).
+port(41, "param", "cassette_id", 71).
+port(42, "param", "sample_id", 72).
+port(43, "param", "frame_number", 73).
+port(44, "in", "total_intensity", 74).
+port(45, "in", "pixel_count", 75).
+port(46, "in", "corrected_image_path", 76).
+port(47, "out", "collection_log", 77).
+port(48, "param", "cassette_id", 82).
+port(49, "param", "sample_id", 83).
+port(50, "param", "num_images", 84).
+port(51, "param", "energies", 85).
+port(52, "param", "image_path_template", 86).
+port(57, "param", "raw_image_path", 93).
+port(58, "param", "corrected_image_path", 94).
+port(59, "param", "calibration_image_path", 95).
 
 % FACT: port_alias(port_id, alias).
-port_alias(31, 'raw_image').
-port_alias(35, 'raw_image').
+port_alias(31, "raw_image").
+port_alias(35, "raw_image").
 
 % FACT: port_uri(port_id, uri).
-port_uri(3, 'file:cassette_{cassette_id}_spreadsheet.csv').
-port_uri(4, 'file:calibration.img').
-port_uri(5, 'file:run/data/{}/{}_{}eV_{}.img').
-port_uri(6, 'file:run/run_log.txt').
-port_uri(7, 'file:run/collected_images.csv').
-port_uri(8, 'file:run/rejected_samples.txt').
-port_uri(9, 'file:run/run_log.txt').
-port_uri(11, 'file:cassette_{cassette_id}_spreadsheet.csv').
-port_uri(23, 'file:/run/rejected_samples.txt').
-port_uri(31, 'file:run/raw/{cassette_id}/{sample_id}/e{energy}/image_{frame_number}.raw').
-port_uri(36, 'file:calibration.img').
-port_uri(37, 'file:data/{sample_id}/{sample_id}_{energy}eV_{frame_number}.img').
-port_uri(47, 'file:run/collected_images.csv').
+port_uri(3, "file:cassette_{cassette_id}_spreadsheet.csv").
+port_uri(4, "file:calibration.img").
+port_uri(5, "file:run/data/{}/{}_{}eV_{}.img").
+port_uri(6, "file:run/run_log.txt").
+port_uri(7, "file:run/collected_images.csv").
+port_uri(8, "file:run/rejected_samples.txt").
+port_uri(9, "file:run/run_log.txt").
+port_uri(11, "file:cassette_{cassette_id}_spreadsheet.csv").
+port_uri(23, "file:/run/rejected_samples.txt").
+port_uri(31, "file:run/raw/{cassette_id}/{sample_id}/e{energy}/image_{frame_number}.raw").
+port_uri(36, "file:calibration.img").
+port_uri(37, "file:data/{sample_id}/{sample_id}_{energy}eV_{frame_number}.img").
+port_uri(47, "file:run/collected_images.csv").
 
 % FACT: has_in_port(block_id, port_id).
 has_in_port(1, 1).
@@ -163,32 +163,32 @@ has_out_port(7, 40).
 has_out_port(8, 47).
 
 % FACT: channel(channel_id, binding).
-channel(1, 'corrected_image').
-channel(2, 'run_log').
-channel(3, 'collection_log').
-channel(4, 'rejection_log').
-channel(5, 'cassette_id').
-channel(6, 'cassette_id').
-channel(7, 'cassette_id').
-channel(8, 'cassette_id').
-channel(9, 'sample_spreadsheet').
-channel(10, 'sample_score_cutoff').
-channel(11, 'sample_name').
-channel(12, 'sample_quality').
-channel(13, 'rejected_sample').
-channel(14, 'accepted_sample').
-channel(15, 'num_images').
-channel(16, 'energies').
-channel(17, 'sample_id').
-channel(18, 'sample_id').
-channel(19, 'energy').
-channel(20, 'frame_number').
-channel(21, 'frame_number').
-channel(22, 'raw_image').
-channel(23, 'calibration_image').
-channel(24, 'total_intensity').
-channel(25, 'pixel_count').
-channel(26, 'corrected_image_path').
+channel(1, "corrected_image").
+channel(2, "run_log").
+channel(3, "collection_log").
+channel(4, "rejection_log").
+channel(5, "cassette_id").
+channel(6, "cassette_id").
+channel(7, "cassette_id").
+channel(8, "cassette_id").
+channel(9, "sample_spreadsheet").
+channel(10, "sample_score_cutoff").
+channel(11, "sample_name").
+channel(12, "sample_quality").
+channel(13, "rejected_sample").
+channel(14, "accepted_sample").
+channel(15, "num_images").
+channel(16, "energies").
+channel(17, "sample_id").
+channel(18, "sample_id").
+channel(19, "energy").
+channel(20, "frame_number").
+channel(21, "frame_number").
+channel(22, "raw_image").
+channel(23, "calibration_image").
+channel(24, "total_intensity").
+channel(25, "pixel_count").
+channel(26, "corrected_image_path").
 
 % FACT: port_connects_to_channel(port_id, channel_id).
 port_connects_to_channel(37, 1).
@@ -206,7 +206,7 @@ port_connects_to_channel(21, 6).
 port_connects_to_channel(1, 7).
 port_connects_to_channel(24, 7).
 port_connects_to_channel(1, 8).
-port_connects_to_channel(41, 8).
+pport_connects_to_channel(41, 8).
 port_connects_to_channel(3, 9).
 port_connects_to_channel(11, 9).
 port_connects_to_channel(2, 10).
@@ -245,12 +245,12 @@ port_connects_to_channel(38, 26).
 port_connects_to_channel(46, 26).
 
 % FACT: uri_variable(uri_variable_id, variable_name, port_id).
-uri_variable(1, 'cassette_id', 3).
-uri_variable(2, 'cassette_id', 11).
-uri_variable(3, 'cassette_id', 31).
-uri_variable(4, 'frame_number', 31).
-uri_variable(5, 'sample_id', 31).
-uri_variable(6, 'energy', 31).
-uri_variable(7, 'frame_number', 37).
-uri_variable(8, 'sample_id', 37).
-uri_variable(9, 'energy', 37).
+uri_variable(1, "cassette_id", 3).
+uri_variable(2, "cassette_id", 11).
+uri_variable(3, "sample_id", 31).
+uri_variable(4, "frame_number", 31).
+uri_variable(5, "cassette_id", 31).
+uri_variable(6, "energy", 31).
+uri_variable(7, "sample_id", 37).
+uri_variable(8, "frame_number", 37).
+uri_variable(9, "energy", 37).
